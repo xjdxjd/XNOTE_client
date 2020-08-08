a82b4133224ca7348a86b8d21103164695d16698
xiaojundi <xiaojundi@scanywhere.com>
2020-08-08 22:19:25 +0800

    添加切面日志handler

--- src/main/java/com/xnote/client/ClientApplication.java
+++ src/main/java/com/xnote/client/ClientApplication.java
@@ -2,8 +2,21 @@ package com.xnote.client;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
+/*public class ClientApplication extends SpringBootServletInitializer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClientApplication.class, args);
+    }
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(ClientApplication.class);
+    }
+}*/
+
 public class ClientApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/xnote/client/core/aop/LogAspectHandler.java
@@ -0,0 +1,9 @@
+package com.xnote.client.core.aop;
+
+/**
+ * AOP 日志手脚架
+ */
+public class LogAspectHandler
+{
+    final
+}
